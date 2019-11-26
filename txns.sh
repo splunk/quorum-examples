ba3f6d4f64f90963348f29f6dd683851f20e7c22
Jeff Wu <jeffreywu@splunk.com>
2019-11-26 11:27:32 -0800

    Fixing brackets

--- txns.sh
+++ txns.sh
@@ -5,8 +5,8 @@ while :
 do
   NODE=$(((RANDOM % 7) + 1))
   echo "Connecting to random node: $NODE"
-  docker exec quorum-examples_node$NODE_1 geth --exec "loadScript('examples/samples/simple-event/private-contract-event.js')" attach qdata/dd/geth.ipc
-  docker exec quorum-examples_node$NODE_1 geth --exec "loadScript('examples/samples/simple-event/public-contract-event.js')" attach qdata/dd/geth.ipc
+  docker exec quorum-examples_node${NODE}_1 geth --exec "loadScript('examples/samples/simple-event/private-contract-event.js')" attach qdata/dd/geth.ipc
+  docker exec quorum-examples_node${NODE}_1 geth --exec "loadScript('examples/samples/simple-event/public-contract-event.js')" attach qdata/dd/geth.ipc
 
   echo "Sleeping 3 secs"
   sleep 3
