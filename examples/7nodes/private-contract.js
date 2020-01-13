c0fcdeb26722f99a497d3e72d1f7340c9a5ef7c8
Jeff Wu <jeffreywu@splunk.com>
2020-01-13 14:42:51 -0800

    Updating bytecode

--- examples/7nodes/private-contract.js
+++ examples/7nodes/private-contract.js
@@ -3,9 +3,36 @@ web3.eth.defaultAccount = a;
 
 // abi and bytecode generated from simplestorage.sol:
 // > solcjs --bin --abi simplestorage.sol
-var abi = [{"constant":true,"inputs":[],"name":"storedData","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"x","type":"uint256"}],"name":"set","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"get","outputs":[{"name":"retVal","type":"uint256"}],"payable":false,"type":"function"},{"inputs":[{"name":"initVal","type":"uint256"}],"payable":false,"type":"constructor"}];
+var abi = [
+	{
+			constant: true,
+			inputs: [],
+			name: 'storedData',
+			outputs: [{ name: '', type: 'uint256' }],
+			payable: false,
+			type: 'function',
+	},
+	{
+			constant: false,
+			inputs: [{ name: 'x', type: 'uint256' }],
+			name: 'set',
+			outputs: [],
+			payable: false,
+			type: 'function',
+	},
+	{
+			constant: true,
+			inputs: [],
+			name: 'get',
+			outputs: [{ name: 'retVal', type: 'uint256' }],
+			payable: false,
+			type: 'function',
+	},
+	{ inputs: [{ name: 'initVal', type: 'uint256' }], payable: false, type: 'constructor' },
+];
 
-var bytecode = "0x6060604052341561000f57600080fd5b604051602080610149833981016040528080519060200190919050505b806000819055505b505b610104806100456000396000f30060606040526000357c0100000000000000000000000000000000000000000000000000000000900463ffffffff1680632a1afcd914605157806360fe47b11460775780636d4ce63c146097575b600080fd5b3415605b57600080fd5b606160bd565b6040518082815260200191505060405180910390f35b3415608157600080fd5b6095600480803590602001909190505060c3565b005b341560a157600080fd5b60a760ce565b6040518082815260200191505060405180910390f35b60005481565b806000819055505b50565b6000805490505b905600a165627a7a72305820d5851baab720bba574474de3d09dbeaabc674a15f4dd93b974908476542c23f00029";
+var bytecode =
+	'0x608060405234801561001057600080fd5b5060405161014c38038061014c8339818101604052602081101561003357600080fd5b8101908080519060200190929190505050806000819055505060f28061005a6000396000f3fe6080604052348015600f57600080fd5b5060043610603c5760003560e01c80632a1afcd914604157806360fe47b114605d5780636d4ce63c146088575b600080fd5b604760a4565b6040518082815260200191505060405180910390f35b608660048036036020811015607157600080fd5b810190808035906020019092919050505060aa565b005b608e60b4565b6040518082815260200191505060405180910390f35b60005481565b8060008190555050565b6000805490509056fea265627a7a7231582071458220641ce1095ca69ab227072e250567877902c6b251fda5a4f2c3fec56c64736f6c63430005100032';
 
 var simpleContract = web3.eth.contract(abi);
 var simple = simpleContract.new(42, {from:web3.eth.accounts[0], data: bytecode, gas: 0x47b760, privateFor: ["ROAZBWtSacxXQrOe3FGAqJDyJjFePR5ce4TSIzmJ0Bc="]}, function(e, contract) {
